b85d42e8aaaa2976de7b901bac46b95cf28031e3
Gabriel Peralta <98152989+itsGabrielPeralta@users.noreply.github.com>
2022-09-24 17:24:34 +0200

    Update main.py

--- BME Negotiations/main.py
+++ BME Negotiations/main.py
@@ -1,19 +1,10 @@
-""" Script to scraping the Bolsa de Madrid web and get company's negotations """
-
-#######################################################
-# LIBRARIES IMPORT                                    #
-#######################################################
-from operator import truediv
+from data_collect import data_collect
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-from openpyxl.workbook import Workbook
 
 
 #######################################################
 # GLOBAL VARIABLES                                    #
 #######################################################
-url = "https://www.bolsamadrid.es/esp/aspx/Empresas/Negociaciones.aspx?ISIN="
 companies_to_scraping = 'empresas.json'
 
 
@@ -24,86 +15,75 @@ companies_to_scraping = 'empresas.json'
 # Load the json file with the companies that we want to scrap
 companies = pd.read_json(companies_to_scraping)
 
-# Process each company allocated into the json file
 for index, company in companies.iterrows():
-
-    # The company ISIN parameter is added to the url to access to the negotiations section in web page
-    individual_url = url + company['isin']
     ticker = company['ticker']
+    # Get the company negotiations
+    df = data_collect(company)
 
-    # Send the request to the web page
-    page = requests.get(individual_url)
 
-    # Check if request is ok
-    if page.status_code != 200:
-        break
 
-    # Define variables to after storage the information
-    df = []
-    progress = ['To the next page']  # This is fill with some silly information to pass the next filter
-
-    # Check if there are a next page. The first attempt always will be it's for this that next variable is completed with silly information
-    while len(progress) > 0:
-        # Parsing the web page with Beutifulsoup library
-        web_page_soup = BeautifulSoup(page.content, 'html.parser')
-
-        # For each Table Row(tr in html) get the Table Data (td in html)
-        for row in web_page_soup.find_all('tr', align="right"):
-            tds = row.find_all('td')
-
-            # Check if tds obtained are greater than 5 because the last column we want is the fifth
-            if len(tds) > 5:
-                # Get the data. The data we want has the format
-                # Hour --> column 0
-                # Prize --> column 1
-                # Volume --> column 2
-                # Operation Id --> column 5
-                df_row = [tds[0].text, tds[1].text, tds[2].text, tds[5].text]
-                # Append this row into the general df where we will get allocated all rows
-                df.append(df_row)
-
-        # Until this we have processed the first page but it's possible that web page has more than 1 page with operations
-        # Get if there are a next button using his element type and element id
-        progress = web_page_soup.find_all('a', id='ctl00_Contenido_SiguientesArr')
-
-        if len(progress) > 0:
-            # Copying the cURL Poxis code for POST request to manually advanced to the next page this is the result
-            # data = {
-            # '__EVENTTARGET': 'ctl00$Contenido$SiguientesArr',
-            # '__EVENTARGUMENT': '',
-            # '__VIEWSTATE': 'kayBDEeOhuKXDdiQaZADRwqbH+u4+yExqxkJQBr3MCirbP+1I8w4w6jMRMYCuBfaGYle5ZPWzEl89AsSnCdZmV8l63+DA8esF7R2c/sCEI4Xb8MBl5ooPaMF1YbxEpjyVabpcVknXVOhoaMXFbRzXJM1avd1QCEwanrfBZUj3qUpsi6y',
-            # '__VIEWSTATEGENERATOR': '8D1B082E',
-            # '__EVENTVALIDATION': 'QvQfGfUpbFcQRALTgiIWJXVep0RaAyNVj9W8Fw8RgXngHAGRsPf3x2ysCA8YOLSwxKgepIYGNnU5dRSN8mxq1eCEiwa3YDLpRzUiyYI/F08MwjvGCLOFHQ81acm8cAa/NNovMDS8PBgAlBlDZBykU/EbALZYbNOqirwpw5vX2Do94v59',
-            # }
-            # We make this code dynamically for get all pages
-
-            __EVENTTARGET = 'ctl00$Contenido$SiguientesArr'
-            __EVENTARGUMENT = ''
-            __VIEWSTATE = web_page_soup.find('input', id='__VIEWSTATE')['value']
-            __VIEWSTATEGENERATOR = web_page_soup.find_all('input', id='__VIEWSTATEGENERATOR')[0]['value']
-            __EVENTVALIDATION = web_page_soup.find_all('input', id='__EVENTVALIDATION')[0]['value']
-
-            payload = {'__EVENTTARGET': __EVENTTARGET, '__EVENTARGUMENT': __EVENTARGUMENT, '__VIEWSTATE': __VIEWSTATE,
-                           '__VIEWSTATEGENERATOR': __VIEWSTATEGENERATOR, '__EVENTVALIDATION': __EVENTVALIDATION}
-            page = requests.post(individual_url, data=payload)
-
-    # Until here we have obtained all data for a company
     # Now set the data with the correct format to english notation for pandas
     df_pandas = pd.DataFrame(df, columns=['Hour', 'Prize', 'Volume', 'Id'])
     df_pandas['Prize'] = pd.to_numeric(df_pandas['Prize'].str.replace(',', '.'), errors='coerce')
     df_pandas['Volume'] = pd.to_numeric(df_pandas['Volume'].str.replace('.', ''), errors='coerce')
-    df_pandas['Id'] = pd.to_numeric(df_pandas['Id'])
-            
+    df_pandas['Id'] = pd.to_numeric(df_pandas['Id'])           
     # Order by id
     df_pandas = df_pandas.sort_values(by=['Id'])
     df_pandas = df_pandas.reset_index(drop=True)
 
-    # Format to date
+
+
+    # Variable definition to storaged the data we want
+    df_same_operation = []
+    start_prize = 0
+    previous_prize = 0
+    volume_sum = 0
+    last_hour = df_pandas['Hour'][0]
+    operations = 0
+    operation_performed = 'Initial Operation'
+
+
+
+    # Process each operation in df_pandas to clean the data in the format we want
+    for index_ops, operation in df_pandas.iterrows():
+        # There are some operations that ocurred in the same milisecond so it's obvious that owns to the same person so accumulate all this operations into one row
+        if last_hour == operation['Hour']:
+            # This is for operations in the same milisecond
+            volume_sum = volume_sum + operation['Volume']
+            previous_prize = operation['Prize']
+            operations = operations + 1
+        else:
+            
+            if start_prize == 0:
+                operation_performed = 'Initial Operation'
+            elif start_prize > previous_prize:
+                operation_performed = 'Buy'
+            elif start_prize < previous_prize:
+
+                operation_performed = 'Sell'
+            # If the prize is the same we guess that is a continuation operation
+
+            # Add the data to a row into the dataframe
+            df_row = [operation_performed, last_hour, previous_prize, volume_sum, operations, previous_prize * volume_sum]
+            df_same_operation.append(df_row)
+
+            # Restart the variables
+            last_hour = operation['Hour']
+            operations = 1
+            volume_sum = operation['Volume']
+            start_prize = previous_prize
+
+    # Record the last operation
+    df_row = ['Final Operation', last_hour, previous_prize, volume_sum, operations, previous_prize * volume_sum]
+    df_same_operation.append(df_row)
+
+    # DataFrame with the operations
+    df_pandas_same_operations = pd.DataFrame(df_same_operation, columns=["Operation", "Hour", "Prize", "Volume", "Num. Operations", "Import"])
+
     day = df_pandas['Hour'][0].split(" ")[0].replace('/', '_')
 
-    # Generate excel file
-    with pd.ExcelWriter('Results/operations' + ticker + '_' + day + '.xlsx') as writer:
+    # Generate the excel file
+    with pd.ExcelWriter('Results/operations_' + ticker + '_' + day + '.xlsx') as writer:
+        df_pandas_same_operations.to_excel(writer, sheet_name='Resume', engine='xlsxwriter')
         df_pandas.to_excel(writer, sheet_name='All', engine='xlsxwriter')
 
-            
-            
